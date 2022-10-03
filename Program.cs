1406878445be5393a99d95df754418c2973d671c
filipesenaifirjan <filipemattossenai@gmail.com>
2022-10-03 09:15:10 -0300

    atribuição

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//nesse caso, esse sinal é de atribuição, pois o sinal e igual atribui o valor para a variável
+
+int a = 10;   
+int b = 20;
+int c = a + b;
+
+Console.WriteLine(c);
