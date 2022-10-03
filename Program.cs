78571c8886812058ec5acc81c058e32469602748
filipesenaifirjan <filipemattossenai@gmail.com>
2022-10-03 10:54:30 -0300

    ordem dos operadores

--- Program.cs
+++ Program.cs
@@ -1,19 +1,8 @@
-﻿
+﻿//double a = 4 / 2 + 2;
 
-//int a = 5;  //aqui está acontecendo um cast implícito
-//double b = a; //o double representa um número muito grande e também representa casas decimais (inteiro cabe no double)
-//Não precisa fazer double b = Convert.ToDouble(a); pois isso seria redundante
-//da mesma forma:
+double a = 4 / (2 + 2);//nesse caso quero que realize a soma primeiro, acrescentando os ()
+Console.WriteLine(a);
 
-//int a = 5;
-//long b = a; //long representa valores em até trilhões
-//Não pode fazer o contrário, pois o int não tem os valores que o long tem, nesse caso vai precisar realizar a conversao de forma manual, ex abaixo:
+//O c# obedece as mesmas ordens da matemática, primeiro vai resolver parênteses, depois expoente, depois divisão e multiplicação e em seguida, adição e subtração
 
-//long a = 5;  //Só não pode usar espaço além do int, por exemplo, não pode usar long.MaxValue; Mas podemos usar int.MaxValue, pois o int cabe dentro do long e o long não cabe dentro do int
-//int b = Convert.ToInt32(a);
-//exemplo de int maxvalue dentro do long
-
-int a = int.MaxValue;//vai imprimir sem erro o max de valor de um int (bilhões)
-long b = a;
-
-Console.WriteLine(b);
+//sempre que for fazer uma operação, prestar atenção nas prioridades
--- README.md
+++ README.md
@@ -60,6 +60,27 @@ Console.WriteLine(a);
 ------------------------------------------------------------------------------------------
 Cast implícito:
 
+//int a = 5;  //aqui está acontecendo um cast implícito
+//double b = a; //o double representa um número muito grande e também representa casas decimais (inteiro cabe no double)
+//Não precisa fazer double b = Convert.ToDouble(a); pois isso seria redundante
+//da mesma forma:
+
+//int a = 5;
+//long b = a; //long representa valores em até trilhões
+//Não pode fazer o contrário, pois o int não tem os valores que o long tem, nesse caso vai precisar realizar a conversao de forma manual, ex abaixo:
+
+//long a = 5;  //Só não pode usar espaço além do int, por exemplo, não pode usar long.MaxValue; Mas podemos usar int.MaxValue, pois o int cabe dentro do long e o long não cabe dentro do int
+//int b = Convert.ToInt32(a);
+//exemplo de int maxvalue dentro do long
+
+int a = int.MaxValue;//vai imprimir sem erro o max de valor de um int (bilhões)
+long b = a;
+
+Console.WriteLine(b);
+----------------------------------------------------------
+Ordem dos operadores:
+
+
 
 
 
