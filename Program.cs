f4548e1ad11f22ee3d4f98fb5fd02c1a28674709
filipesenaifirjan <filipemattossenai@gmail.com>
2022-10-03 10:16:46 -0300

    diferença entre Convert e parse

--- Program.cs
+++ Program.cs
@@ -1,12 +1,3 @@
-﻿//int a = 5;  //Ele não vai reconhecer se o valor for declarado como int e estiver entre "5", pois em "" só deve se colocar string ou 5a, 5b, nesse caso tbm deixa de ser número inteiro.
-// Cast ou Casting faz a conversão
-//Convert é uma classe
-//Toint32 é um método que recebe uma string e transforma em int
-//exemplos abaixo:
-
-//int a = Convert.ToInt32("5");
-
-int a = int.Parse("5"); //o parse converte a representação de string para 32bit de inteiro, mas se vc colocar 5c vai dar erro pq não vai conseguir ler 
-//todos os tipos podem ser convertidos através do convert ou parse e esse processo chamamos de cast ou casting. Mas tem que tomar cuidado pq nem sempre vem como se espera( lembra que o usuário pode e vai te surpreender)
+﻿int a = Convert.ToInt32(null);//nesse caso, um dato vazio é representado através do null, pois o null converte o valor (vazio) para zero e não vai dar erro e nesse caso não pode usar o parse pq ele não vai aceitar que seja nulo.
 
 Console.WriteLine(a);
