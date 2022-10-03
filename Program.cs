8de057879d671825612ef2b5a8222970d70e99ae
filipesenaifirjan <filipemattossenai@gmail.com>
2022-10-03 10:26:25 -0300

    conversão para string

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
-﻿int a = Convert.ToInt32(null);//nesse caso, um dato vazio é representado através do null, pois o null converte o valor (vazio) para zero e não vai dar erro e nesse caso não pode usar o parse pq ele não vai aceitar que seja nulo.
+﻿//Não existe parse para string
+
+int inteiro = 5;  //aqui tenho o valor inteiro
+string a = inteiro.ToString(); //Aqui tenho o valor inteiro que foi atribuido e convertido para string
+
+
+//O ToString é muito comum no c# e ele vem de uma classe mais genérica do c# chamada object (mãe de todas as classes) 
+//Todo tipo pode ser representado em string e a gente usa o ToString
 
 Console.WriteLine(a);
--- README.md
+++ README.md
@@ -40,3 +40,11 @@ Console.WriteLine(a);
 -------------------------------------------------------------------------
 diferença entre parse e convert:
 
+int a = Convert.ToInt32(null);//nesse caso, um dato vazio é representado através do null, pois o null converte o valor (vazio) para zero e não vai dar erro e nesse caso não pode usar o parse pq ele não vai aceitar que seja nulo.
+
+Console.WriteLine(a);
+---------------------------------------------------------------------------
+Conversão para string:
+
+
+
